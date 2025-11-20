1445a9c95e7db1c30844bdd97c3b47c96a1fdaa1
Bonface-3020 <thuobonface42@gmail.com>
2025-11-20 13:02:51 +0300

    changed home to index

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.home, name='Home'),
+    path('', views.index, name='Home'),
     path('about/',views.about,name='About'),
     path('products/',views.products,name='Products'),
     path('services/',views.services,name='Services'),
